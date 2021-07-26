5396df9fece4340f21763ade13127b7801aa2779
MngSebastian <m.sebi97@yahoo.com>
2021-07-26 16:02:53 +0200

    Working on the Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+
+            <div className='home-btn'>
+                <p>THE PLANETS</p>
+            </div>
+
+            <div className='planets-list'>
+                <ul>
+                    <li>Mercury</li>
+                    <li>Venus</li>
+                    <li>Earth</li>
+                    <li>Mars</li>
+                    <li>Jupiter</li>
+                    <li>Saturn</li>
+                    <li>Uranus</li>
+                    <li>Neptune</li>
+                </ul>
+            </div>
+
+
+
+
+
+            
+        </div>
+    )
+}
+
+export default Navbar
