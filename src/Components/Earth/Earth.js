013805fdf1ff45ffff246c45c9406678d08cfed6
MngSebastian <m.sebi97@yahoo.com>
2021-10-18 03:42:02 +0200

    Further improvements have been made to the navbar, font size for title and planet buttons has been decreased and the title has been vertically readjusted. Planet buttons horizontally re-adjusted.

--- src/Components/Earth/Earth.js
+++ src/Components/Earth/Earth.js
@@ -11,21 +11,25 @@ function Earth() {
                     <img src={earth} alt="Picture of Earth" />
                 </div>
                 <div className='planet-info'>
-                    <h1 className='planet-name'>EARTH</h1>
-                    <p className='planet-info-text'>Third planet from the Sun and the only known planet to harbor life. About 29.2%
+                    <div className='left'>
+                        <h1 className='planet-name'>EARTH</h1>
+                        <p className='planet-info-text'>Third planet from the Sun and the only known planet to harbor life. About 29.2%
                         of Earth's surface is land with remaining 70.8% is covered with water.
                         Earth's distance from the Sun, physical properties and geological history have allowed
                         life to evolve and thrive.</p>
-                    <p>Source: <a className='wikipedia' href='#'>Wikipedia <img src={Source} /> </a></p>
-                    <form method="get" action="/earth">
-                        <button type="submit" className='planet-btn overview active-earth'><p>01</p>Overview</button>
-                    </form>
-                    <form method="get" action="/earth-internal">
-                        <button className='planet-btn structure'><p>02</p>Internal Structure</button>
-                    </form>
-                    <form method="get" action="/earth-geology">
-                        <button className='planet-btn geology'><p>03</p>Surface Geology</button>
-                    </form>
+                        <p>Source: <a className='wikipedia' href='#'>Wikipedia <img src={Source} /> </a></p>
+                    </div>
+                    <div className='right'>
+                        <form method="get" action="/earth">
+                            <button type="submit" className='planet-btn overview active-earth'><p>01</p>Overview</button>
+                        </form>
+                        <form method="get" action="/earth-internal">
+                            <button className='planet-btn structure'><p>02</p>Internal Structure</button>
+                        </form>
+                        <form method="get" action="/earth-geology">
+                            <button className='planet-btn geology'><p>03</p>Surface Geology</button>
+                        </form>
+                    </div>
                 </div>
             </div>
             <div className='footer'>
